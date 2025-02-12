5e8ea53619aebbb8b5e93383075962e37a4afe4b
maria <maria.junqueira@einstein.br>
2025-02-12 11:45:36 -0300

    refactor: delete file

--- src/workers/pdf-worker.js
+++
@@ -1,7 +0,0 @@
-import { pdfjsLib } from 'pdfjs-dist';
-import pdfWorker from 'pdfjs-dist/build/pdf.worker.min.mjs?worker';
-
-// Definir o worker manualmente
-pdfjsLib.GlobalWorkerOptions.workerSrc = pdfWorker;
-
-export default pdfjsLib;
